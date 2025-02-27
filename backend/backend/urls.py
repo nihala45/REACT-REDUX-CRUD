e79f3af0fb2511307789e02d3d9186f51b4a32dc
nihala shirin <nihalashirin02@gmail.com>
2025-02-27 14:37:10 +0530

    profile update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('api/',include('api.urls')),
     path('api-auth/', include('rest_framework.urls'))
     
-]
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
